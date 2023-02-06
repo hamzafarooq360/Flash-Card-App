6f017b34117f5f4eb79f6bbf36d37853a94ad7d2
Hamza Farooq <hamzafarooq360@gmail.com>
2023-02-05 23:54:51 -0500

    decks on landing page

---
+++ client/src/App.tsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+type TDeck = {
+  title: string;
+  _id: string;
+}
+
+function App() {
+
+  const [title, setTitle] = useState("")
+  const [decks, setDecks] = useState<TDeck[]>([])
+
+  async function handleCreateDeck(e: React.FormEvent){
+    e.preventDefault()
+    await fetch('http://localhost:3000/decks', {
+      method: "POST",
+      body: JSON.stringify({title,
+      }),
+      headers: {
+        "content-type": "application/json",
+      }
+
+  });
+  setTitle("")
+  }
+
+  useEffect(() => {
+    async function fetchDecks() {
+      const response = await fetch("http://localhost:3000/decks")
+      const newDecks = await response.json();
+      setDecks(newDecks);
+  
+    }
+    fetchDecks();
+  }, []);
+
+  return (
+    <div className="App">
+      <ul className="decks">
+        {decks.map((deck) => (
+            <li key={deck._id}>{deck.title}</li>
+          ))}
+      </ul>
+          
+      <form onSubmit={handleCreateDeck}>
+        <label htmlFor="deck-title">Deck Title</label>
+        <input 
+          id="deck-title"
+          value={title} 
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            setTitle(e.target.value);
+          }}
+        />
+        <button>Create Deck</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
