527d289c7610fc8e1bd48f11d7506f6b2d7feb79
Hamza Farooq <hamzafarooq360@gmail.com>
2023-02-07 04:19:19 -0500

    created controllers and refactored code

--- client/src/Deck.tsx
+++ client/src/Deck.tsx
@@ -1,11 +1,62 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { Link } from 'react-router-dom';
+import { deleteDeck } from './api/deleteDeck';
+import { TDeck, getDecks } from './api/getDecks';
+import { createDeck } from './api/createDeck';
 
 const Deck = () => {
-  return (
-    <div>
-        Deck
-    </div>
-  )
+    const [title, setTitle] = useState("")
+    const [decks, setDecks] = useState<TDeck[]>([])
+  
+    async function handleCreateDeck(e: React.FormEvent){
+      e.preventDefault();
+      const deck = await createDeck(title);
+      setDecks([...decks, deck]);
+      setTitle("");
+    }
+  
+    async function handleDeleteDeck(deckId: string) {
+      await deleteDeck(deckId);
+      setDecks(decks.filter((deck) => deck._id !== deckId))
+  
+    }
+  
+    useEffect(() => {
+      async function fetchDecks() {
+        const newDecks = await getDecks()
+        setDecks(newDecks);
+    
+      }
+      fetchDecks();
+    }, []);
+  
+    return (
+      <div className="App">
+        <ul className="decks">
+          {decks.map((deck) => (
+              <li key={deck._id}>
+                <button className="delete-button" onClick={() => handleDeleteDeck(deck._id)}>X</button>
+                <Link to={`decks/${deck._id}`}>{deck.title}</Link>
+              </li>
+            ))}
+        </ul>
+            
+        <form onSubmit={handleCreateDeck}>
+          <label htmlFor="deck-title">Deck Title</label>
+          <input 
+            id="deck-title"
+            value={title} 
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setTitle(e.target.value);
+            }}
+          />
+          <button>Create Deck</button>
+        </form>
+      </div>
+    )
 }
 
 export default Deck
